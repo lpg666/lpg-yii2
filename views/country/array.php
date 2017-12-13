0bfa5645cc9ddacc7b1270c8c021e3e2e63a7d19
lpg <1003289700@qq.com>
2017-12-13 18:03:44 +0800

    学习助手类

--- views/country/array.php
+++ views/country/array.php
@@ -5,7 +5,29 @@ use yii\helpers\ArrayHelper;
 
 $this->title = 'Array助手';
 $this->params['breadcrumbs'][] = $this->title;
+
+class User
+{
+    public $name = 'Alex';
+}
+
+$array = [
+    'foo' => [
+        'bar' => new User(),
+    ]
+];
 ?>
     <h2>Array助手demo</h2>
 
-    <!--1-->
+    <!--获取值-->
+        <!--点号分割的数组键名或对象属性组成的字符串-->
+        <?= ArrayHelper::getValue($array, 'foo.bar.name', '默认为null'); ?>
+        <!--返回一个值的回调函数-->
+        <?php
+            $fullName = ArrayHelper::getValue($array, function ($array, $defaultValue) {
+                return $array['foo']['bar']->name;
+            });
+            echo '<br/>'.$fullName.'<br/>';
+        ?>
+        <!--取值后立刻删除-->
+        <?= $type = ArrayHelper::remove($array, 'type'); ?>
